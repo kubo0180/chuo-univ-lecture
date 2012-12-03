90d8e461401f36fc1b7deed6ba3fe5e94336c8de
kubo0180 <gibson02122569@gmail.com>
2012-12-03 14:57:15 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChuoU::Application.config.secret_token = '341c39f04818b9b512bb21ef3e19d57bdd83341285a6470b8134180fd148e8b1fc3a2e368c296cd94d7876dff878fb8d168c0705a98fce1226826fe63f84abc2'
